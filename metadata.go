89db98d3c67ce0860ee77897259951e0669a04df
Alexander F. Rødseth <rodseth@gmail.com>
2021-11-11 15:00:32 +0100

    Remove a comment

--- metadata.go
+++ metadata.go
@@ -1,23 +1,6 @@
 package heic
 
 /*
- * Copyright (c) 2021 Alexander F. Rødseth <xyproto@archlinux.org>
- *
- * heif is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * heif is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with heic.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-/*
 #cgo pkg-config: libheif
 #include <libheif/heif.h>
 #include <stdlib.h>
